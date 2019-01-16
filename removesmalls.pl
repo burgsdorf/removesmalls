6ca2294b4178981732ba2d1020c01874ef50597e
burgsdorf <30334981+burgsdorf@users.noreply.github.com>
2019-01-16 21:01:16 +0200

    Add files via upload
    The author of the script is unknown. Submitted as supplementary material to the scientific manuscript.

---
+++ removesmalls.pl
@@ -0,0 +1,20 @@
+## removesmalls.pl
+## The author of the script is unknown. Submitted as a supplementary material for the scientific manuscript. 
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $minlen = shift or die "Error: `minlen` parameter not provided\n";
+{
+    local $/=">";
+    while(<>) {
+        chomp;
+        next unless /\w/;
+        s/>$//gs;
+        my @chunk = split /\n/;
+        my $header = shift @chunk;
+        my $seqlen = length join "", @chunk;
+        print ">$_" if($seqlen >= $minlen);
+    }
+    local $/="\n";
+}
